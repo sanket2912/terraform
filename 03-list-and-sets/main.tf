fad4bb6a7c24b81906d6a58a63b81c83865ae62e
sanket2912 <dumbresanket29@gmail.com>
2022-10-06 14:56:14 +0530

    using set to solve problem with list.

--- 03-list-and-sets/main.tf
+++ 03-list-and-sets/main.tf
@@ -16,6 +16,8 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "my_iam_user" {
-  count = length(var.names)
-  name  = var.names[count.index]
+  #count = length(var.names)
+  #name = var.names[count.index]
+  for_each  = toset(var.names)
+  name = each.value
 }
